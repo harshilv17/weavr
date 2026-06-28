2abc4c53aa8cc7781aa2771fe26d46640072a335
harshilv17 <harshilvalecha@gmail.com>
2026-06-28 16:15:00 +0530

    chore(server): add project config

---
+++ app/server/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  clearMocks: true,
+  setupFiles: ["<rootDir>/tests/setupEnv.ts"],
+};
+
+export default config;
